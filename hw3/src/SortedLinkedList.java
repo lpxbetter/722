0ad76cb22e17d27572e08422ac978f8624386ce4
Liping Xiong <marrowxiong@gmail.com>
2015-11-19 10:00:53 -0500

    hw3 update

--- hw3/src/SortedLinkedList.java
+++ hw3/src/SortedLinkedList.java
@@ -1,19 +1,52 @@
 /**
- * Created by lipingxiong on 11/17/15.
- */
+* Andrew ID: lipingx
+* @author liping xiong
+*/
 
 public class SortedLinkedList implements MyListInterface {
+    /**
+     *
+     *  @param
+     */
+    final static boolean debug = false;
+    /**
+     * Node structure of the linked list
+     */
     class Node {
         String val;
         Node next;
         Node(String val){
             this.val = val;
+            this.next = null;
         }
     }
+    /**
+     * head of the linked list
+     */
     private Node head;
-
+    /**
+     * No-arg constructor
+     */
     SortedLinkedList(){
         head = null;
+    }
+    /**
+     * Constructor with an array of string as parameter
+     * @param unsorted an unsorted array of string
+     */
+    SortedLinkedList(String[] unsorted){
+        head = null;
+        build(unsorted, 0);
+    }
+    /**
+     * Innner function called by Constructor
+     *  @param strs unsorted array of string
+     *  @param i Index of current word
+     */
+    private void build(String[] strs,int i){
+        if(i == strs.length) return;
+        add(strs[i]);
+        build(strs,i+1);
     }
 
     /**
@@ -21,17 +54,34 @@ public class SortedLinkedList implements MyListInterface {
      * No duplicates allowed and maintain the order in ascending order.
      * @param value String to be added.
      */
-    /*
-    null
+    private void add(String value,Node pre,Node front){
+        // Add to the end of the list
+        if(front == null) {
+            Node newNode = new Node(value);
+            pre.next = newNode;
+            return;
+        }
 
-    // insert to head, need to update head
-    "" -    b -
-    |       |
-    pre    cur
-      c
+        if(front.val.equals(value) ){
+            return;
+        }
+        // Insert before the node "front"
+        if(front.val.compareTo(value) > 0){
+            Node newNode = new Node(value);
+            newNode.next = front;
+            pre.next = newNode;
+            if(front == head) head = newNode; // update head
+        }
+        else{ // continue to find the insertion position
+            add(value, pre.next,front.next);
+        }
+    }
+    /**
+     * Inserts a new String.
+     * No duplicates allowed and maintain the order in ascending order.
+     * @param value String to be added.
      */
     public void add(String value){
-        // is empty
         if(head == null) {
             Node newNode = new Node(value); // New one node
             head = newNode;
@@ -39,61 +89,48 @@ public class SortedLinkedList implements MyListInterface {
         }
 
         Node dummy = new Node("");
-        Node cur = head;
+        dummy.next = head;
+        Node front = head;
         Node pre = dummy;
-        while(cur != null){
-            // duplicate
-            if(cur.val.compareTo(value) == 0 ){
-                return;
-            }
 
-            // Found the place that the value should be inserted
-            if(cur.val.compareTo(value) > 0){
-                // New one node and inert to between pre and cur
-                Node newNode = new Node(value); // New one node
-                newNode.next = cur;
-                pre.next = newNode;
-                // Insert to the front, need to update the head
-                if(head == cur){
-                    head = newNode;
-                }
-            }
-            cur = cur.next;
-            pre = pre.next;
-        }
-
-        // insert to the end
-        if(cur ==  null){
-            Node newNode = new Node(value); // New one node
-            pre.next = newNode;
-        }
-
+        add(value, pre, front);
     }
 
     /**
      * Checks the size (number of data items) of the list.
      * @return the size of the list
      */
-
     public int size(){
-        int n = 0;
-        Node cur = head;
-        while(cur != null){
-            cur = cur.next;
-            n++;
-        }
-        return n;
+        return size(head);
+    }
+    /**
+     * Checks the size (number of data items) of the list.
+     * @return the size of the list
+     */
+    private int size(Node front){
+        if(front == null) return 0;
+        return 1 + size(front.next);
     }
     /**
      * Displays the values of the list.
      */
     public void display(){
-        if(this.isEmpty()) return;
-        Node cur = head;
-        while(cur != null){
-            System.out.println(cur.val+",");
-        }
+//        if(head == null) return;
+        System.out.print("[");
+        display(head);
+        System.out.print("]");
         System.out.println();
+    }
+    /**
+     * Displays the values of the list.
+     */
+    private void display(Node front){
+        if(front == null) {
+            return;
+        }
+        if(front.next == null) System.out.print(front.val);
+        else System.out.print(front.val+", ");
+        display(front.next);
     }
 
     /**
@@ -101,17 +138,18 @@ public class SortedLinkedList implements MyListInterface {
      * @param key String key to search
      * @return true if found, false if not found
      */
-    /*
-    a-b-c-d
-     */
     public boolean contains(String key){
-        if(this.isEmpty()) return false;
-        Node cur = head;
-        while(cur != null){
-            if(cur.val.equals(key)) return true;
-            cur = cur.next;
-        }
-        return false;
+        return contains(key,head);
+    }
+    /**
+     * Returns true if the key value is in the list.
+     * @param key String key to search
+     * @return true if found, false if not found
+     */
+    private boolean contains(String key, Node front){
+        if(front == null) return false;
+        if(front.val.equals(key)) return true;
+        return contains(key,front.next);
     }
 
     /**
@@ -127,7 +165,10 @@ public class SortedLinkedList implements MyListInterface {
      * @return String object that is removed
      */
     public String removeFirst(){
-
+        if(head == null) return "";
+        String res = head.val;
+        head = head.next;
+        return res;
     }
 
     /**
@@ -136,8 +177,28 @@ public class SortedLinkedList implements MyListInterface {
      * @return String object that is removed
      */
     public String removeAt(int index){
+        if(index >= this.size()) return "";
+        //throw new IllegalArgumentException("Index should be less than the size of the list.");
+        if(index == 0 ) return removeFirst();
 
+        Node pre = new Node("");
+        pre.next = head;
+        // pre is the previous node of node AT index
+        return removeAt(index,-1,pre); // index of pre, and pre node
     }
-
+    /**
+     * Removes and returns String object at the specified index.
+     * @param index index to remove String object
+     * @return String object that is removed
+     */
+    private String removeAt(int index, int preIdx, Node pre){
+        String res = "";
+        if(preIdx+1 == index) {
+            res = pre.next.val;
+            pre.next = pre.next.next;// remove the node
+            return  res;
+        }
+        return removeAt(index,preIdx+1,pre.next);
+    }
 
 }
